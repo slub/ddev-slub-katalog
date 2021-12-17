4befce4e7a173d783f877567251c0ec777c3af70
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-12-17 14:44:10 +0100

    Enable debug mode

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'BE' => [
-        'debug' => false,
+        'debug' => true,
         'explicitADmode' => 'explicitAllow',
         'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$ZGN4aWtDOHFPQzZwNXpCbA$IkeeVhk1xpKNy+NWg8QL9HXrVVtKUVdA/66KSR7JLVM',
         'loginSecurityLevel' => 'normal',
@@ -20,7 +20,8 @@ return [
     ],
     'EXT' => [
         'extConf' => [
-            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
+            'backend' => 'a:6:{s:9:"loginLogo";s:0:"";s:19:"loginHighlightColor";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}',
+            'extension_builder' => 'a:3:{s:15:"enableRoundtrip";s:1:"1";s:15:"backupExtension";s:1:"1";s:9:"backupDir";s:35:"uploads/tx_extensionbuilder/backups";}',
             'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
             'scheduler' => 'a:2:{s:11:"maxLifetime";s:4:"1440";s:15:"showSampleTasks";s:1:"1";}',
             'solr' => 'a:4:{s:35:"useConfigurationFromClosestTemplate";s:1:"0";s:43:"useConfigurationTrackRecordsOutsideSiteroot";s:1:"1";s:29:"useConfigurationMonitorTables";s:0:"";s:27:"allowSelfSignedCertificates";s:1:"0";}',
@@ -42,6 +43,11 @@ return [
             'loginHighlightColor' => '',
             'loginLogo' => '',
         ],
+        'extension_builder' => [
+            'backupDir' => 'uploads/tx_extensionbuilder/backups',
+            'backupExtension' => '1',
+            'enableRoundtrip' => '1',
+        ],
         'extensionmanager' => [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
@@ -58,9 +64,25 @@ return [
         ],
     ],
     'FE' => [
+        'cacheHash' => [
+            'excludedParameters' => [
+                'L',
+                'pk_campaign',
+                'pk_kwd',
+                'utm_source',
+                'utm_medium',
+                'utm_campaign',
+                'utm_term',
+                'utm_content',
+                'gclid',
+                'fbclid',
+                'tx_slubevents_apieventlist',
+            ],
+        ],
         'cookieDomain' => 'ddev.site',
-        'debug' => false,
+        'debug' => true,
         'loginSecurityLevel' => 'normal',
+        'pageNotFoundOnCHashError' => false,
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
@@ -82,7 +104,7 @@ return [
                     'writerConfiguration' => [
                         5 => [
                             'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                'disabled' => true,
+                                'disabled' => false,
                             ],
                         ],
                     ],
@@ -99,15 +121,15 @@ return [
         'transport_smtp_username' => '',
     ],
     'SYS' => [
-        'devIPmask' => '',
-        'displayErrors' => 0,
+        'devIPmask' => '*',
+        'displayErrors' => 1,
         'encryptionKey' => '710830ffde66a45f68282df96f60b810561b381f4ea96fc9be52caf7c562bbbee46d2879e7a2d1807776193bfa9b99b5',
-        'exceptionalErrors' => 4096,
+        'exceptionalErrors' => 12290,
         'features' => [
             'newTranslationServer' => true,
             'unifiedPageTranslationHandling' => true,
         ],
         'sitename' => 'New TYPO3 site',
-        'systemLogLevel' => 2,
+        'systemLogLevel' => 0,
     ],
 ];
